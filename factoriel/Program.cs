802b64232c743bbbba029e338fe74498213d446a
LOQ <LOQ@DESKTOP-FUMHBU1>
2023-11-01 11:28:19 +0330

    Add project files.

---
+++ factoriel/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, i;
+long fact = 1;
+Console.WriteLine("yek adad baraye factoril vared konid");
+n = int.Parse(Console.ReadLine());
+for (i = n; i >= 1; i--)
+{
+    fact *= i;
+}
+Console.WriteLine("factoril= " + fact);
+Console.ReadKey();
+
+// AMIRHOSSEIN AZARAKHSH .tamrine 5//
